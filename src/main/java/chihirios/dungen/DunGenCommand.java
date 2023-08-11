2bf83336ae103a2e52ac37e0070b5dff00f40afd
Jack Pearce <jack@jackf.red>
2023-08-11 03:13:36 +0100

    [feat] initial dungeon gen

--- src/main/java/chihirios/dungen/DunGenCommand.java
+++ src/main/java/chihirios/dungen/DunGenCommand.java
@@ -1,6 +1,12 @@
 package chihirios.dungen;
 
 import chihirios.dungen.generator.DunGenData;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -14,7 +20,14 @@ public class DunGenCommand {
                         .then(literal("generate")
                                 .executes(ctx -> {
                                     // generate new dungeon and take player there
-                                    DunGen.LOGGER.info("" + DunGenData.get(ctx.getSource().getServer()).increment());
+                                    var index = DunGenData.get(ctx.getSource().getServer()).increment();
+                                    var dungeonDim = ctx.getSource().getServer().getLevel(ResourceKey.create(Registries.DIMENSION, new ResourceLocation("dun_gen", "dungeon")));
+                                    var structure = ctx.getSource().getServer().getStructureManager().get(new ResourceLocation("dun_gen", "start")).orElse(null);
+                                    if (dungeonDim == null || structure == null) return 0;
+                                    var pos = new BlockPos(index * 1000, 0, 0);
+                                    // var bounds = structure.getBoundingBox(pos, Rotation.NONE, pos, Mirror.NONE);
+                                    structure.placeInWorld(dungeonDim, pos, pos, new StructurePlaceSettings(), ctx.getSource().getLevel().random, Block.UPDATE_ALL);
+                                    ctx.getSource().getPlayerOrException().teleportTo(dungeonDim, pos.getX() + 3, pos.getY() + 1, pos.getZ() + 3, 0f, 0f);
                                     return 0;
                                 }))
         );
