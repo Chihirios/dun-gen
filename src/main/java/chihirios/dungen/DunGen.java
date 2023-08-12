d684e9221f7fbb9a6fc5e30f7e671306979c83da
Jack Pearce <jack@jackf.red>
2023-08-12 02:09:39 +0100

    [feat] data gen for template pools

--- src/main/java/chihirios/dungen/DunGen.java
+++ src/main/java/chihirios/dungen/DunGen.java
@@ -1,18 +1,25 @@
 package chihirios.dungen;
 
+import chihirios.dungen.datagen.DunGenDataGen;
 import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DunGen.MODID)
-public class DunGen
-{
+public class DunGen {
     public static final String MODID = "dun_gen";
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MODID, path);
+    }
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public DunGen() {
         MinecraftForge.EVENT_BUS.register(DunGenCommand.class);
+        FMLJavaModLoadingContext.get().getModEventBus().register(DunGenDataGen.class);
     }
 }
---
+++ src/main/java/chihirios/dungen/datagen/DunGenDataGen.java
@@ -0,0 +1,59 @@
+package chihirios.dungen.datagen;
+
+import chihirios.dungen.DunGen;
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.Pools;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.Set;
+import java.util.function.Function;
+
+public class DunGenDataGen {
+    private static ResourceKey<StructureTemplatePool> pool(String path) {
+        return ResourceKey.create(Registries.TEMPLATE_POOL, DunGen.id(path));
+    }
+
+    private static Pair<Function<StructureTemplatePool.Projection, ? extends StructurePoolElement>, Integer> template(String path, int weight) {
+        return Pair.of(StructurePoolElement.single(DunGen.id(path).toString()), weight);
+    }
+
+    @SubscribeEvent
+    public static void doDataGen(GatherDataEvent event) {
+        event.getGenerator().addProvider(event.includeServer(), DunGenDataGen.createProvider(event));
+    }
+
+    public static DataProvider.Factory<DatapackBuiltinEntriesProvider> createProvider(GatherDataEvent event) {
+        return output -> new DatapackBuiltinEntriesProvider(output,
+                event.getLookupProvider(),
+                new RegistrySetBuilder().add(Registries.TEMPLATE_POOL, DunGenDataGen::makePools),
+                Set.of(DunGen.MODID)
+        );
+    }
+
+    private static void makePools(BootstapContext<StructureTemplatePool> bootstrap) {
+        HolderGetter<StructureTemplatePool> templatePools = bootstrap.lookup(Registries.TEMPLATE_POOL);
+        Holder<StructureTemplatePool> emptyPool = templatePools.getOrThrow(Pools.EMPTY);
+
+        bootstrap.register(pool("dungeon/red"), new StructureTemplatePool(
+                emptyPool,
+                ImmutableList.of(
+                        template("red1", 2),
+                        template("red2", 4),
+                        template("red3", 1)
+                ),
+                StructureTemplatePool.Projection.RIGID
+        ));
+    }
+}
